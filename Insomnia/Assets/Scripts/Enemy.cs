bcf10e104c1bc858f18a55940048ad500db97c7a
Pierre Ferrari <pierreferrari22@gmail.com>
2022-02-26 19:46:58 +0100

    God, point de vie, systeme de combats, mage...

--- Insomnia/Assets/Scripts/Enemy.cs
+++ Insomnia/Assets/Scripts/Enemy.cs
@@ -1,28 +1,37 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   [SerializeField] private GameObject _enemyPrefab;
    [SerializeField] private GameObject _cannon;
-
-   public int uniteLife;
-   [SerializeField] private float _uniteSpeed;
-   [SerializeField] private float _uniteMeleDamage;
-   [SerializeField] private float _uniteRangeDamage;
-   [SerializeField] private float _uniteRange;
+   
    [SerializeField] private bool isMele;
+
+   [Header("Statistiques de l'ennemie")]
+   [SerializeField] private int uniteLife;
+   [SerializeField] private float _uniteSpeed;
+   
+   [SerializeField] private int _uniteMeleDamage;
+   [SerializeField] private int _uniteRangeDamage;
+   
+   [SerializeField] private float _uniteRange;
    private bool _enemyIsInRange;
    private float distance;
-
    
+   [Header("Cooldown")]
+   [SerializeField] private float strikeCd;
+   [SerializeField] private float shootCd;
+   private bool canEngage;
 
-   public bool isFromPlayer1;
+   private GameObject target;
    
    Rigidbody2D rb;
+   [SerializeField] private bool canMove;
 
    private void Awake()
    {
-      if (isFromPlayer1)
+      if (GameManager.instance.isFromPlayer1)
       {
          gameObject.tag = "IsFromPlayer1";
          gameObject.layer = 6;
@@ -35,35 +44,64 @@ public class Enemy : MonoBehaviour
       }
       
       rb = gameObject.GetComponent<Rigidbody2D>();
+      canEngage = true;
+      canMove = true;
    }
 
    private void Update()
    {
+      if (!canMove)
+      {
+         rb.constraints = RigidbodyConstraints2D.FreezeAll;
+      }
+      
       if (gameObject.tag == "IsFromPlayer1")
       {
          rb.velocity = Vector2.right * _uniteSpeed;
       }
       else
       {
-         rb.velocity = Vector2.left * _uniteSpeed;
+            rb.velocity = Vector2.left * _uniteSpeed;
       }
-
+      
+      
       if (_enemyIsInRange)
       {
+         canMove = false;
+         
          if (isMele)
          {
-           Strike();
+            StartCoroutine(Strike());
          }
          else
          {
             if (distance <= 1)
             {
-               Strike();
+               StartCoroutine(Strike());
             }
             else
             {
-               Shoot();
+               StartCoroutine(Shoot());
             }
+         }
+      }
+      else
+      {
+         canMove = true;
+      }
+
+      if (uniteLife <= 0)
+      {
+         Destroy(gameObject);
+         if (gameObject.tag == "IsFromPlayer1")
+         {
+            GameManager.instance.player2Gold += 10;
+            GameManager.instance.UpdateUI();
+         }
+         else
+         {
+            GameManager.instance.player1Gold += 10;
+            GameManager.instance.UpdateUI();
          }
       }
    }
@@ -75,23 +113,62 @@ public class Enemy : MonoBehaviour
 
       if (hit2D.collider != null)
       {
-            distance = Mathf.Abs(hit2D.point.y - transform.position.y); // Distance entre l'unité et l'ennemie touché
-            _enemyIsInRange = true;
+         if (hit2D.collider.gameObject.tag != gameObject.tag)
+         {
+             distance = Mathf.Abs(hit2D.point.y - transform.position.y); // Distance entre l'unité et l'ennemie touché
+             _enemyIsInRange = true;
+             target = hit2D.collider.gameObject;
+         }
+         else
+         {
+            _enemyIsInRange = false;
+         }
       }
    }
 
-   public void SpawnUnite(Vector3 spawnPos)
+   IEnumerator Strike()
    {
-      Instantiate(_enemyPrefab,spawnPos, Quaternion.identity);
+      if (canEngage)
+      {
+         if (target == GameManager.instance.basePlayer1)
+         {
+            GameManager.instance.basePlayer1Life -= _uniteMeleDamage;
+         }
+         else if (target == GameManager.instance.basePlayer2)
+         {
+            GameManager.instance.basePlayer2Life -= _uniteMeleDamage;
+         }
+         else
+         {
+            target.GetComponent<Enemy>().uniteLife -= _uniteMeleDamage;
+         }
+         canEngage = false;
+         GameManager.instance.UpdateUI();
+         yield return new WaitForSeconds(strikeCd);
+         canEngage = true;
+      }
    }
 
-   void Strike()
+   IEnumerator Shoot()
    {
-      Debug.Log(("Strike"));
-   }
-
-   void Shoot()
-   {
-      Debug.Log(("Shoot"));
+      if (canEngage)
+      {
+         if (target == GameManager.instance.basePlayer1)
+         {
+            GameManager.instance.basePlayer1Life -= _uniteRangeDamage;
+         }
+         else if (target == GameManager.instance.basePlayer2)
+         {
+            GameManager.instance.basePlayer2Life -= _uniteRangeDamage;
+         }
+         else
+         {
+            target.GetComponent<Enemy>().uniteLife -= _uniteRangeDamage;
+         }
+         canEngage = false;
+         GameManager.instance.UpdateUI();
+         yield return new WaitForSeconds(shootCd);
+         canEngage = true;
+      }
    }
 }
--- Insomnia/Assets/Scripts/GameManager.cs
+++ Insomnia/Assets/Scripts/GameManager.cs
@@ -1,16 +1,32 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    
-    public Enemy enemy;
+
+    public GameObject[] enemyPrefabs; // knight = 1, mage = 2
     
     public GameObject spawnPosPlayer1;
     public GameObject spawnPosPlayer2;
 
     public GameObject basePlayer1;
     public GameObject basePlayer2;
+
+    public Slider lifebarPlayer1;
+    public Slider lifebarPlayer2;
+    
+    public int basePlayer1Life;
+    public int basePlayer2Life;
+
+    public int player1Gold;
+    public int player2Gold;
+
+    public TextMeshProUGUI p1GoldText;
+    public TextMeshProUGUI p2GoldText;
+
+    [HideInInspector] public bool isFromPlayer1;
 
     private void Awake()
     {
@@ -20,24 +36,60 @@ public class GameManager : MonoBehaviour
             return;
         }
         instance = this;
+
+        lifebarPlayer1.maxValue = basePlayer1Life;
+        lifebarPlayer2.maxValue = basePlayer2Life;
+        
+        UpdateUI();
     }
     
     void Update()
     {
-        GatherInputs();
+        if (basePlayer1Life == 0)
+        {
+            Debug.Log("Player2 won");
+        }
+        if (basePlayer2Life == 0)
+        {
+            Debug.Log("Player1 won");
+        }
     }
 
-    void GatherInputs()
+    public void UpdateUI()
     {
-        if (Input.GetKeyDown("a"))
-        {
-            enemy.isFromPlayer1 = true;
-            enemy.SpawnUnite(spawnPosPlayer1.transform.position);
-        }
-        if (Input.GetKeyDown("e"))
-        {
-            enemy.isFromPlayer1 = false;
-            enemy.SpawnUnite(spawnPosPlayer2.transform.position);
-        }
+        lifebarPlayer1.value = basePlayer1Life;
+        lifebarPlayer2.value = basePlayer2Life;
+
+        p1GoldText.text = player1Gold.ToString();
+        p2GoldText.text = player2Gold.ToString();
+    }
+    
+    public void SpawnKnightPlayer1()
+    {
+        isFromPlayer1 = true;
+        Instantiate(enemyPrefabs[0],spawnPosPlayer1.transform.position, Quaternion.identity);
+        player1Gold -= 10;
+        UpdateUI();
+    }
+    public void SpawnKnightPlayer2()
+    {
+        isFromPlayer1 = false;
+        Instantiate(enemyPrefabs[0],spawnPosPlayer2.transform.position, Quaternion.identity);
+        player2Gold -= 10;
+        UpdateUI();
+    }
+    public void SpawnMagePlayer1()
+    {
+        isFromPlayer1 = true;
+        Instantiate(enemyPrefabs[1],spawnPosPlayer1.transform.position, Quaternion.identity);
+        player1Gold -= 10;
+        UpdateUI();
+    }
+    public void SpawnMagePlayer2()
+    {
+        isFromPlayer1 = false;
+        Instantiate(enemyPrefabs[1],spawnPosPlayer2.transform.position, Quaternion.identity);
+        player2Gold -= 10;
+        UpdateUI();
     }
 }
